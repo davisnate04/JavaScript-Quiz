41e63f24a0cda74351c1c586eb747674316c3197
davisnate04 <davisnate04@gmail.com>
2023-12-18 20:22:47 -0500

    Added the score saving system

--- assets/scripts/getScore.js
+++ assets/scripts/getScore.js
@@ -1,3 +1,33 @@
 const totalScore = document.getElementsByClassName("totalScore");
+const input = document.getElementById("scoreInput");
+const button = document.getElementById("submit");
+const score = localStorage.getItem("Time");
+const highScoreDefault = localStorage.setItem("highScore", []);
 
-totalScore.textContent = localStorage.getItem("Time");
+const highScore = JSON.parse(localStorage.getItem("highScore") || []);
+
+for (const child of totalScore) {
+    child.textContent = score;
+}
+
+function addInitials() {
+if (input.textContent = "") {
+    alert("Invalid Username");
+} else {
+    const newScore = {
+        score: score,
+        intials: input.value
+    };
+
+    highScore.push(newScore);
+
+    highScore.sort((a,b) => b.score - a.score);
+
+    highScore.splice(5);
+
+    localStorage.setItem("highScore", JSON.stringify(highScore));
+    }
+}
+
+
+button.addEventListener("click", addInitials);
