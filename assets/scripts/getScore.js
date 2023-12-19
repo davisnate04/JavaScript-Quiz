dc44ddeb8380522a0705d62ec7686186cbb65a61
davisnate04 <davisnate04@gmail.com>
2023-12-19 14:46:55 -0500

    Testing quiz

--- assets/scripts/getScore.js
+++ assets/scripts/getScore.js
@@ -2,21 +2,15 @@ const totalScore = document.getElementsByClassName("totalScore");
 const input = document.getElementById("scoreInput");
 const button = document.getElementById("submit");
 const score = localStorage.getItem("Time");
-const highScoreDefault = localStorage.setItem("highScore", []);
-
 const highScore = JSON.parse(localStorage.getItem("highScore") || []);
-
-for (const child of totalScore) {
-    child.textContent = score;
-}
 
 function addInitials() {
 if (input.textContent = "") {
     alert("Invalid Username");
 } else {
     const newScore = {
-        score: score,
-        intials: input.value
+        "score": score,
+        "intials": input.value
     };
 
     highScore.push(newScore);
@@ -26,8 +20,19 @@ if (input.textContent = "") {
     highScore.splice(5);
 
     localStorage.setItem("highScore", JSON.stringify(highScore));
+
+    input.value = "";
+    window.location.assign('index.html');
     }
 }
 
+window.addEventListener('load', function() {
+    if (highScore === null) {
+    const highScoreDefault = localStorage.setItem("highScore", JSON.stringify([]));
+    }
 
+    for (const child of totalScore) {
+        child.textContent = score;
+    }
+});
 button.addEventListener("click", addInitials);
