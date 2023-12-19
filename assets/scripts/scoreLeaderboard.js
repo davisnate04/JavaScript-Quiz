414414c1fccb04b329b04865271fa247525e51a0
davisnate04 <davisnate04@gmail.com>
2023-12-19 15:12:22 -0500

    Testing the quiz again

--- assets/scripts/scoreLeaderboard.js
+++ assets/scripts/scoreLeaderboard.js
@@ -2,7 +2,6 @@ const list = document.getElementById('highScoreList');
 const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
 const resetHighScore = document.getElementById("resetHighScore");
 
-
 list.innerHTML = highScore.map(newScore => {
     return `<li class="scoreListItem">${newScore.intials} - ${newScore.score}</li>`;
 }).join("");
